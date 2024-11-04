9737caa819e13122eed12a3549d44e292bf66446
HamiltonChris <chris.ross.hamilton@gmail.com>
2024-11-03 20:04:00 -0800

    Initial commit

---
+++ tests/mpu6050Test.cpp
@@ -0,0 +1,24 @@
+extern "C"
+{
+#include "mpu6050.h"
+}
+
+#include "CppUTest/TestHarness.h"
+
+TEST_GROUP(MPU6050)
+{
+    void setup()
+    {
+
+    }
+
+    void teardown()
+    {
+
+    }
+};
+
+TEST(MPU6050, init)
+{
+    FAIL("Initial failing test.");
+}
